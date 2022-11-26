d5422814983e9c2e8c6e0535a30555862df7a315
Lipton Barua <liptonbarua274@gmail.com>
2022-11-26 23:14:17 +0600

    project-10

--- src/Pages/BookingCollection/Bookings.js
+++ src/Pages/BookingCollection/Bookings.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
 
 const Bookings = () => {
+    const {data:booking =[]} = useQuery({
+        queryKey: ['booking'],
+        queryFn: async()=>{
+            const res= await fetch('http://localhost:8000/booking')
+            const data= await res.json();
+            return data;
+        }
+    })
     return (
         <div>
-            <h1>This is a book</h1>
+          <div className="overflow-x-auto">
+  <table className="table table-zebra w-full">
+
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Product</th>
+        <th>Price</th>
+        <th>Location</th>
+      </tr>
+    </thead>
+    <tbody>
+    
+     {
+        booking.map((booking,i)=> <tr>
+            <th>{i+1}</th>
+            <th>{booking.name}</th>
+            <td>{booking.title}</td>
+            <td>{booking.price}</td>
+            <td>{booking.location}</td>
+          </tr>)
+     }
+    </tbody>
+  </table>
+</div>
         </div>
     );
 };
