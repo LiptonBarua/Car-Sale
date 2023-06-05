742668da43fe8a03d985690392468c10e9cba7ea
Lipton Barua <liptonbarua274@gmail.com>
2023-06-05 09:54:40 +0600

    project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./Components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  darkMode: 'class',
   theme: {
     extend: {},
   },
